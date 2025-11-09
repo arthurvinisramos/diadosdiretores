95c05f33a5b53d0f87da5abc25e79cd2a49031c5
Arthur622352 <arthurviniciussramos@gmail.com>
2025-11-09 20:35:33 -0300

    oi

---
+++ script.js
@@ -0,0 +1,12 @@
+const botao = document.getElementById('botao');
+const musica = document.getElementById('musica');
+
+botao.addEventListener('click', () => {
+  
+  musica.play();
+
+  window.scrollTo({
+    top: document.body.scrollHeight,
+    behavior: 'smooth'
+  });
+});
